c9ab57c9de8af534175964f0a7c1b109819db0dd
umarfikri <umarfikri1712@gmail.com>
2022-12-22 04:16:22 +0800

    Create Login page
    Fail to load at browser

---
+++ application/controllers/Home.php
@@ -0,0 +1,11 @@
+<?php
+class Home extends CI_Controller {    
+
+    function index (){
+        $data['pagetitle'] = 'Login';
+        $data['module'] = 'login';
+        $data['submodule'] = '';        
+
+        $this->load->view('login', $data);
+    }
+}
