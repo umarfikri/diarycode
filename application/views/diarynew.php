0f259307ae15f9e575169911ef849e8780cd14c6
umarfikri <115766951+umarfikri@users.noreply.github.com>
2022-12-23 15:26:39 +0800

    Edit a bit form

--- application/views/diarynew.php
+++ application/views/diarynew.php
@@ -1,52 +1,26 @@
-<div class="card mt-4" id="basic-info">
-            <div class="card-header">
-              <h5>Aduan Baharu</h5>
-            </div>
-            <div class="card-body pt-0">
-            <form method="POST" enctype="multipart/form-data">
-              <div class="row">
-                <div class="col-6">
-                  <label class="form-label">Tajuk</label>
-                  <div class="input-group">
-                    <input id="tajuk" name="tajuk" class="form-control" type="text" placeholder="Tajuk" required="required" onfocus="focused(this)" onfocusout="defocused(this)">
-                  </div>
-                </div>
-                <div class="col-6">
-                  <label class="form-label">Tarikh</label>
-                  <div class="input-group">
-                    <input id="tarikh" name="tarikhaduan" class="form-control" type="date" placeholder="date" required="required" value="<?= date("Y-m-d") ?>" onfocus="focused(this)" onfocusout="defocused(this)" readonly>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-12 col-12">
-                  <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Deskripsi</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" name="deskripsi" required="required" rows="3"></textarea>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-12 col-12">
-                  <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Justifikasi</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" name="justifikasi" required="required" rows="3"></textarea>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-6">
-                  <label class="form-label">File</label>
-                  <div class="input-group">
-                    <input id="file" name="attachment" class="form-control" type="file" placeholder="date" onfocus="focused(this)" onfocusout="defocused(this)">
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-12 col-12">
-                  <button class="btn bg-gradient-dark btn-sm float-end mt-6 mb-0" value="submit" name="submit">Hantar</button>
-                </div>
-              </div>
-            </form>
-            </div>
-          </div>
+<div class="card position-absolute">
+  <div class="card-body">
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-check">
+            <label class="form-check-label">
+                <input class="form-check-input" type="checkbox" value="">
+                Option one is this
+                <span class="form-check-sign">
+                    <span class="check"></span>
+                </span>
+            </label>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</div>
