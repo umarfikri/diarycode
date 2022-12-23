c26186f5c07dc57bcd585ceb32cf525138b6fb9f
umarfikri <115766951+umarfikri@users.noreply.github.com>
2022-12-23 15:11:43 +0800

    Debug css file error

--- application/views/header.php
+++ application/views/header.php
@@ -3,6 +3,7 @@
 <html lang="en">
 
 <head>
+  <base href="<?php echo base_url(); ?>" />
   <meta charset="utf-8" />
   <link rel="apple-touch-icon" sizes="76x76" href="assets/img//apple-icon.png">
   <link rel="icon" type="image/png" href="assets/img//favicon.png">
