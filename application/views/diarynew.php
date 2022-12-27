0df5e18232b2b89a2e6d837f9d92e1957d5f8add
umarfikri <115766951+umarfikri@users.noreply.github.com>
2022-12-27 08:51:07 +0800

    Diary New interface

--- application/views/diarynew.php
+++ application/views/diarynew.php
@@ -1,31 +1,39 @@
+<div class="page-header" style="background-image: url('assets/img/login-image.jpg');">
+    <div class="card mt-4" style="width: 70rem;">
+        <div class="card-body pt-0">
+            <div class="container position-relative">
+                <h5 class="text-center mt-4">New Post</h5>
+                <div class="row">
+                    <div class="col-md-12 ml-auto mr-auto">
+                        <form>
+                            <div class="form-group">
+                                <label>Title</label>
+                                <input type="text" class="form-control" id="title" placeholder="Title">                                
+                            </div>
+                            <div class="form-group">
+                                <label>Post</label>
+                                <textarea class="form-control" id="postpage" rows="3"></textarea>                                
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            </div>
+                            <div class="form-check">
+                                <label class="form-check-label">
+                                    <input class="form-check-input" type="checkbox" value="">
+                                    Option one is this
+                                    <span class="form-check-sign">
+                                        <span class="check"></span>
+                                    </span>
+                                </label>
+                            </div>
 
-<div class="container position-relative">
-    <h5 class="text-center">New Post</h5>
-    <div class="row">
-        <div class="col-md-8 ml-auto mr-auto">
-            <form>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <div class="form-check">
-                    <label class="form-check-label">
-                        <input class="form-check-input" type="checkbox" value="">
-                        Option one is this
-                        <span class="form-check-sign">
-                            <span class="check"></span>
-                        </span>
-                    </label>
-                </div>
-
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
-    </div>
+            </div>
+        </div>        
+    </div>    
 </div>
   
