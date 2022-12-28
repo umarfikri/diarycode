705df37913faccaafc20d5f58b32b3bf092dccb7
umarfikri <115766951+umarfikri@users.noreply.github.com>
2022-12-28 16:48:40 +0800

    Change diary new

--- application/views/diarynew.php
+++ application/views/diarynew.php
@@ -1,11 +1,11 @@
-<div class="main">
-    <div class="page-header"  data-parallax="true" style="background-image: url('assets/img/login-image.jpg');">    
-        <div class="container" style="margin-top: 6rem!important;">
+
+<div class="page-header"  data-parallax="true" style="background-image: url('assets/img/login-image.jpg');">
+    <div class="container" style="margin-top: 6rem!important;">        
             <div class="row">
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="text-center mt-4">New Post</h5>
+                            <h5 class="text-center">New Post</h5>
                             <div class="row">
                                 <div class="col-md-12 ml-auto mr-auto">
                                     <form>
@@ -17,7 +17,7 @@
                                         <div class="form-group">
                                             <label>Content</label>
                                             <div class="col-sm-12">                                
-                                            <textarea class="form-control" name="content" id="content" style="height: 100px"></textarea>
+                                            <textarea class="form-control" name="content" id="content" style="height: 50px"></textarea>
                                             <script>
                                                 // Replace the <textarea id="editor1"> with a CKEditor 4
                                                 // instance, using default configuration.
@@ -84,8 +84,8 @@
                         </div>        
                     </div>
                 </div>
-            </div>        
-        </div>       
-    </div>
+            </div>           
+    </div>        
 </div>
+
   
