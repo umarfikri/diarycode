702ab2322d6ebc8b7b2b1ac522ab839a59614844
umarfikri <115766951+umarfikri@users.noreply.github.com>
2023-01-11 16:25:46 +0800

    Test Calendar
    Without php

---
+++ application/views/calendar.php
@@ -0,0 +1,132 @@
+<div class="section section-dark text-center">
+      <div class="container">
+        <h2 class="title">Mood Calendar</h2>
+        <div class="row">
+        <table class="table table-striped table-dark">
+            <thead class="thead-light">
+                <tr>
+                <th>Day / Month</th>
+                <th>January</th>
+                <th>February</th>
+                <th>March</th>
+                <th>April</th>        
+                <th>May</th>
+                <th>June</th>
+                <th>August</th>
+                <th>September</th>
+                <th>October</th>
+                <th>September</th>
+                <th>December</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td class="bg-primary">Mood Sad</td>
+                  <td class="bg-warning">Mood Happy</td>
+                  <td class="bg-light">Unavailable</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td class="bg-secondary">Mood Tired</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Larry</td>
+                  <td>the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+                <tr>
+                  <th scope="row">4</th>
+                  <td>Larry</td>
+                  <td>the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">10</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">15</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">20</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">25</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">3</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">30</th>               
+                </tr>
+                <tr> 
+                  <th scope="row">31</th>               
+                </tr>
+            </tbody>
+        </table>
+        </div>
+      </div>
+    </div>
