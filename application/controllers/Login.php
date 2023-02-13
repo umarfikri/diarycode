6a030fc19a9a35be0dafa654957df437fcbc237c
umarfikri <umarfikri1712@gmail.com>
2023-02-14 05:37:58 +0800

    Update Login session

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,7 +4,37 @@ class Login extends CI_Controller {
     function index (){
         $data['pagetitle'] = 'Login';
         $data['module'] = 'login';
-        $data['submodule'] = '';        
+        $data['submodule'] = '';  
+        
+        if($this->input->post('submit')){
+            // print_r($this->input->post());
+            $userid = $this->input->post('username');
+            $password = $this->input->post('password');
+            $userinfo = $this->db->query("SELECT * FROM profile WHERE username = '$userid'")->row();
+            // print_r("<pre>");
+            // print_r($userinfo);      
+            // die();      
+			
+            if (!empty($userinfo)) {
+                if ($userinfo->password == $password) {
+                    $variable_session = array(
+                        'uID'  => $userinfo->username,
+                        'uName'     => $userinfo->fullname
+                    );
+                    $this->session->set_userdata($variable_session);
+                    redirect('home/index');
+                } else {
+                    $this->session->set_flashdata('fail', 'Kata Laluan Tidak Sama');
+                    redirect('login/index');
+                }
+                
+				
+			}
+			else {
+                $this->session->set_flashdata('fail', 'Username Tidak Wujud');
+                redirect('login/index');
+			}
+		}
 
         $this->load->view('login', $data);
     }
@@ -28,7 +58,12 @@ class Login extends CI_Controller {
                         
         }
 
-   
         $this->load->view('register', $data);
+    }
+
+    function logout (){
+
+        $this->session->sess_destroy();
+		redirect('login/index');
     }
 }
